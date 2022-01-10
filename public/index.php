4820b19bd3f744e50fdd5d3d657ffadc7f6b68aa
Serhii Samson <raffenkaf@gmail.com>
2022-01-10 20:26:39 +0200

    adjustments for heroku

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
-var_dump('www');
+
 session_start();
 
 if (file_exists(__DIR__ . '/../.env')) {
@@ -14,9 +14,9 @@ if (file_exists(__DIR__ . '/../.env')) {
 } else {
     throw new Exception('Env file should be created.');
 }
-var_dump('www');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
+error_reporting(getenv('PHP_ERROR_REPORTING_LEVEL'));
+ini_set('display_errors', getenv('PHP_INI_SET_DISPLAY_ERRORS'));
 date_default_timezone_set(getenv('PHP_DATE_DEFAULT_DEFAULT_TIMEZONE_SET'));
 
 $containerBuilder = new ContainerBuilder();
