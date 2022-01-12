14a8341b3f9cbc40e1a8c5b84dd26dece943e6fd
Serhii Samson <raffenkaf@gmail.com>
2022-01-12 10:30:12 +0200

    change readme

--- db/city.sql
+++ db/city.sql
@@ -1,7 +1,7 @@
-INSERT INTO test.city (id, name) VALUES (1, 'Киев');
-INSERT INTO test.city (id, name) VALUES (2, 'Одесса');
-INSERT INTO test.city (id, name) VALUES (3, 'Москва');
-INSERT INTO test.city (id, name) VALUES (4, 'Харьков');
-INSERT INTO test.city (id, name) VALUES (5, 'Чернигов');
-INSERT INTO test.city (id, name) VALUES (6, 'Париж');
-INSERT INTO test.city (id, name) VALUES (7, 'Москва');
+INSERT INTO test.city (id, name) VALUES (1, 'Kyiv');
+INSERT INTO test.city (id, name) VALUES (2, 'Odessa');
+INSERT INTO test.city (id, name) VALUES (3, 'Moskva');
+INSERT INTO test.city (id, name) VALUES (4, 'Kharkov');
+INSERT INTO test.city (id, name) VALUES (5, 'Chernigov');
+INSERT INTO test.city (id, name) VALUES (6, 'Paris');
+INSERT INTO test.city (id, name) VALUES (7, 'Donetsk');
