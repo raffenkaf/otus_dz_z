1360ab00e6b86910f34d3ef451bfd30ded09a3f4
Serhii Samson <s.samson@booknet.com>
2022-02-07 06:20:43 +0200

    Homework №2. add report

---
+++ 2nd-dz-report/few_indexes/alter_table.sql
@@ -0,0 +1,5 @@
+create index user_first_name_index
+	on user (first_name);
+
+create index user_last_name_index
+	on user (last_name);
---
+++ 2nd-dz-report/multiply_column_index/alter_table.sql
@@ -0,0 +1,2 @@
+create index user_first_name_last_name_index
+    on user (first_name, last_name);
